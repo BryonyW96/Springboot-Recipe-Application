6d1f5b9b6438de43bd49efcdd752bd1486ef4494
BryonyW96 <bryonywilso@hotmail.com>
2024-06-09 15:49:57 +0100

    added in some css to index page

--- spring-recipe-application/src/main/java/bryony/applications/spring_recipe_application/RestControllers/WebController.java
+++ spring-recipe-application/src/main/java/bryony/applications/spring_recipe_application/RestControllers/WebController.java
@@ -1,10 +1,11 @@
 package bryony.applications.spring_recipe_application.RestControllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import bryony.applications.spring_recipe_application.POGOS.Recipe;
@@ -16,10 +17,18 @@ public class WebController {
     @Autowired
     private RecipeServiceImpl recipeServiceImpl; 
 
-    @RequestMapping(method = RequestMethod.GET, value = "/get", produces="application/json")
-    public String saveRecipe(@RequestParam String name) {
-        System.out.println(name);
-        return name; 
+    @RequestMapping(method = RequestMethod.POST, value = "/saveRecipe", produces="application/json")
+    public void saveRecipe(@RequestBody Recipe recipe) {
+        recipeServiceImpl.save(recipe); 
     }
+
+    @RequestMapping(method = RequestMethod.GET, value="/getAllRecipes", produces="application/json")
+    public List<Recipe> getAllRecipes() {
+        return recipeServiceImpl.getAllRecipes(); 
+    }
+
+   
+
+    
     
 }
